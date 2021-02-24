d04941ffab81171d586ad6acf519f82b6b41af35
bukkun <bukkun.tkb@gmail.com>
2021-02-24 22:12:39 +0900

    remove io.open() in the lua file

--- lua_tutorial_4/FTPUpload.lua
+++ lua_tutorial_4/FTPUpload.lua
@@ -7,7 +7,6 @@ All sample code on this page is licensed under BSD 2-Clause License
 https://github.com/FlashAirDevelopers/LuaTutorial/blob/master/LICENSE
 ]]
 
-local logfile   = "/FTPLog.txt"     -- Log file created in FlashAir
 local folder    = "/Upload"         -- Folder to upload file is located
 local server    = "192.168.1.1"     -- IP address of FTP server
 local serverDir = "/LuaTutorial"    -- FTP server upload folder
@@ -18,34 +17,29 @@ local passwd    = "abc123"          -- FTP password
 -- example: "ftp://user:pass@192.168.1.1/LuaTutorial"
 local ftpstring = "ftp://"..user..":"..passwd.."@"..server..serverDir
 
--- Open the log file
-local outfile = io.open(logfile, "w")
-
--- Write a header
-outfile:write("File list: \n")
-
 -- For each file in folder...
 for file in lfs.dir(folder) do
+    -- Skip dot files
+    if string.sub(file,1,1) == "." then
+        goto continue
+    end
     -- Get that file's attributes
     attr = lfs.attributes(folder .. "/" .. file)
-    print( "Found "..attr.mode..": " .. file )
+--    print( "Found "..attr.mode..": " .. file )
 
     -- Don't worry about directories (yet)
     if attr.mode == "file" then
         --Attempt to upload the file!
         --ex ftp("put", "ftp://user:pass@192.168.1.1/LuaTutorial/test.jpg", "Upload/test.jpg")
-        response = fa.ftp("put", ftpstring .. "/" .. file, folder .. "/" .. file)
+        response = fa.ftp("put", ftpstring .. "/" .. attr.modification .. "_" .. file, folder .. "/" .. file)
 
         --Check to see if it worked, and log the result!
         if response ~= nil then
-            print("Success!")
-            outfile:write("" .. file .. "... Success!\n")
+--              print("Success!")
         else
-            print("Fail :(")
-            outfile:write("" .. file .. "... Fail :(\n")
+--              print("Fail :(")
         end
     end
-end
 
---Close our log file
-outfile:close()
+    ::continue::
+end
